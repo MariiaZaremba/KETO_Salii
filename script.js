2fdf1b67c041298a69026d5819b33b1a901fa26a
Mariia Zaremba <164428922+MariiaZaremba@users.noreply.github.com>
2026-06-11 19:59:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzqff59sQiDqr_SotdVfvb3mHCutXOThv-wJBQpskIRAt46785xJZ2tMMOHLB9p_4FfdA/exec";
 
+
 const params = new URLSearchParams(window.location.search);
 const chatIdFromUrl = params.get("chat_id");
 
